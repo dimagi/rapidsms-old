c59a45c88b4d9255d63cd92b6d336536ade11aa0
Cory Zue <czue@dimagi.com>
2009-06-24 15:32:43 -0400

    hiding the rapidsms dashboard for hq

--- apps/webapp/urls.py
+++ apps/webapp/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.defaults import *
 import views as views
 
 urlpatterns = patterns('',
-    url(r'^$',     views.dashboard),
+#    url(r'^$',     views.dashboard),
     url(r'^ping$', views.check_availability),
     (r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'webui/login.html'}),
     (r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'template_name': 'webui/loggedout.html'}),
