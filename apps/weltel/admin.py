930ad01321d36cfb0cfe2aa8db84cc7d2b487e04
U-rotop\rowena <rowena@rotop.(none)>
2009-09-03 10:37:28 -0400

    add patient state model

--- apps/weltel/admin.py
+++ apps/weltel/admin.py
@@ -2,8 +2,9 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 
 from django.contrib import admin
-from weltel.models import Site, Nurse, Patient
+from weltel.models import Site, Nurse, Patient, PatientState
 
 admin.site.register(Site)
 admin.site.register(Nurse)
 admin.site.register(Patient)
+admin.site.register(PatientState)
--- apps/weltel/models.py
+++ apps/weltel/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 from reporters.models import Reporter, PersistantConnection, Location 
 from locations.models import Location
@@ -23,11 +24,20 @@ GENDER_CHOICES=(
     (FEMALE,'female')
 )
 
+class PatientState(models.Model):
+    code = models.CharField(max_length=15)
+    name = models.CharField(max_length=63, null=True, blank=True)
+    description = models.CharField(max_length=255, null=True, blank=True)
+    
+    def __unicode__(self):
+        return self.name if self.name else self.code
+
 class Patient(Reporter):
     """ This model represents a patient for WelTel """
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, null=True)
     locations = models.ForeignKey(Site, null=True)
     default_connection = models.ForeignKey(PersistantConnection, null=True)
+    state = models.ForeignKey(PatientState, default=PatientState.objects.get(code='default') )
     
     # make 'patient_id' an alias for reporter.alias
     def _get_patient_id(self):
@@ -35,3 +45,18 @@ class Patient(Reporter):
     def _set_patient_id(self, value):
         self.alias = value
     patient_id = property(_get_patient_id, _set_patient_id)
+
+# an event log showing when patients states were manually transitioned
+# (i.e. when a nurse submitted a follow up report that resulted in a
+# state transition)
+class PatientStateLog(models.Model):
+    patient = models.ForeignKey(Patient)
+    transitioned_to = models.ForeignKey(PatientState, null=True)
+    # typically, nurses will trigger transitions
+    # but one can imagine an administrator doing it via a webui or something
+    by = models.CharField(max_length=63, null=True)
+    date = models.DateTimeField(null=False, default=datetime.now() )
+    def __unicode__(self):
+        return self.name if self.name else self.code
+    # TODO - link this to patient.save()
+
