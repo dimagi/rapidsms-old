477eb224131658241b83d6393c6eb0f870c468a6
czue <czue@dimagi.com>
2009-11-12 17:18:33 -0500

    adding "container" classes to the section divs on the rapidsms pages, so we can do some global styling trickery.

--- apps/schools/views.py
+++ apps/schools/views.py
@@ -1,4 +1,13 @@
 from django.http import HttpResponse
+from rapidsms.webui.utils import render_to_response
 
 def schools(req):
     return HttpResponse("Schools!")
+
+def map(req, template_name="schools/school_map.html"):
+    """Display a map with school data on it"""
+    return render_to_response(req, template_name, {})
+
+def xml(req, template_name="schools/demo.xml"):
+    """Get some basic xml data for populating the map."""
+    return render_to_response(req, template_name, {})
