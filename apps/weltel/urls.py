f8ad89ffdb4bac9afb436e3d64654fe60c07f27d
U-rotop\rowena <rowena@rotop.(none)>
2010-01-16 17:11:15 +0200

    Add a link to view of raw patient message logs

--- apps/weltel/urls.py
+++ apps/weltel/urls.py
@@ -10,8 +10,8 @@ from weltel.forms import PatientForm, NurseForm
 urlpatterns = patterns('',
     url(r'^$',                      views.index),
     url(r'^site/(?P<pk>\d+)$',             views.site),
-    url(r'^patient/(?P<pk>\d+)$',          views.patient),
-    url(r'^patient/(?P<pk>\d+)/messages$', views.patient_messages),
+    url(r'^patient/(?P<pk>\d+)$',          views.patient, name="patient_history"),
+    url(r'^patient/(?P<pk>\d+)/messages$', views.patient_messages, name="patient_messages"),
     url(r'^nurse/(?P<pk>\d+)$',            views.nurse),
     url(r'^patient/(?P<pk>\d+)/edit$',     views.edit_klass, \
         {'klass':Patient, 'klass_form':PatientForm}),
