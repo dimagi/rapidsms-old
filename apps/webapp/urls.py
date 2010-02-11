7539d95d91c6991014471910d0e376c044c857dc
czue <czue@dimagi.com>
2010-02-11 15:22:59 -0500

    Adding names to the login/logout views so they can be referenced by reverse() and {% url %}

--- apps/webapp/urls.py
+++ apps/webapp/urls.py
@@ -10,8 +10,8 @@ from rapidsms.webui import settings
 urlpatterns = patterns('',
     url(r'^$',     views.dashboard),
     url(r'^ping$', views.check_availability),
-    (r'^accounts/login/$', views.login, {"template_name": settings.LOGIN_TEMPLATE }),
-    (r'^accounts/logout/$', views.logout, {"template_name": settings.LOGGEDOUT_TEMPLATE }),
+    url(r'^accounts/login/$', views.login, {"template_name": settings.LOGIN_TEMPLATE }, name="login"),
+    url(r'^accounts/logout/$', views.logout, {"template_name": settings.LOGGEDOUT_TEMPLATE }, name="logout"),
     url(r'^i18n/', include('django.conf.urls.i18n')),
 )
 
