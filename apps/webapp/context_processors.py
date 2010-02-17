9b1eb7c84570ddea2ad275afee69f79ee015fa2e
czue <czue@dimagi.com>
2010-02-17 17:31:28 -0500

    In the context_processor file, importing settings from django.conf instead of rapidsms world.
    http://docs.djangoproject.com/en/dev/topics/settings/#using-settings-in-python-code

--- apps/webapp/context_processors.py
+++ apps/webapp/context_processors.py
@@ -1,9 +1,9 @@
+from django.conf import settings
+
 def base_template( request ):
     """This sticks the base_template variable defined in the settings
        into the request context, so that we don't have to do it in 
        our render_to_response override."""
-    from rapidsms.webui import settings
-    
     return {"base_template" : settings.BASE_TEMPLATE}
     
 
