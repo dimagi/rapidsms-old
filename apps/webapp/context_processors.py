d50f0f628351884185d7f40331ea786938313b78
czue <czue@dimagi.com>
2010-02-17 16:41:10 -0500

    changing the settings import path in the rapidsms context_processor

--- apps/webapp/context_processors.py
+++ apps/webapp/context_processors.py
@@ -2,7 +2,8 @@ def base_template( request ):
     """This sticks the base_template variable defined in the settings
        into the request context, so that we don't have to do it in 
        our render_to_response override."""
-    import settings
+    from rapidsms.webui import settings
+    
     return {"base_template" : settings.BASE_TEMPLATE}
     
 
