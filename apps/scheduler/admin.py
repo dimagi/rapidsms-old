31ffa55ee7def045a7aeca7daa76e6a504d583a5
Rowena <rluk@dimagi.com>
2010-02-20 20:03:19 -0500

    UI to edit schedules for Scheduler app (cherry picked from commit 83010a19df5dab7ed0cd3f09ed73563903d157a9)

--- apps/scheduler/admin.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
-
-from django.contrib import admin
-from scheduler.models import EventSchedule
-
-admin.site.register(EventSchedule)
