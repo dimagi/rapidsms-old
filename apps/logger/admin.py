8ee71818515deea8b5fadf0f0d36f6dcc934a09a
Rowena <rluk@dimagi.com>
2010-05-01 10:59:13 -0400

    Remove IncomingMessage and OutgoingMessage from admin page and display only Message (since the other two seem to be deprecated)

--- apps/logger/admin.py
+++ apps/logger/admin.py
@@ -2,19 +2,6 @@
 # vim: ai ts=4 sts=4 et sw=4
 
 from django.contrib import admin
-from models import OutgoingMessage, IncomingMessage
+from models import Message
 
-
-class OutgoingMessageAdmin(admin.ModelAdmin):
-    list_display = ['identity', 'backend', 'text', 'sent']
-    list_filter = ['identity', 'backend', 'text', 'sent']
-    date_hierarchy = 'sent'
-
-class IncomingMessageAdmin(admin.ModelAdmin):
-    list_display = ['identity', 'backend', 'text', 'received']
-    list_filter = ['identity', 'backend', 'text', 'received']
-    date_hierarchy = 'received'
-
-
-admin.site.register(OutgoingMessage, OutgoingMessageAdmin)
-admin.site.register(IncomingMessage, IncomingMessageAdmin)
+admin.site.register(Message)
