1abb2ad3e892ce1508dab754e8d1d309782be58e
rowena <rowena@ronux.(none)>
2010-07-25 23:48:34 -0400

    One better: report patients with activity from 'today'

--- apps/weltel/callbacks.py
+++ apps/weltel/callbacks.py
@@ -1,5 +1,5 @@
 import logging
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 from rapidsms.message import Message
 from rapidsms.i18n import ugettext_noop as _
 from logger.models import IncomingMessage
@@ -24,10 +24,8 @@ def shida_report(router, nurse=None):
         shida = PatientState.objects.get(code='shida')
         # get all active patients who responded shida or are in the default state
         patients = Patient.objects.filter(site=site).filter(state=shida).exclude(active=False).exclude(subscribed=False)
-        # only get patients with recent activity (last 12 hours)
-        timeout_interval = timedelta(hours=12)
-        timeout = datetime.now() - timeout_interval
-        patients = patients.filter(eventlog__date__gt=timeout).distinct()
+        # only get patients with activity from today
+        patients = patients.filter(eventlog__date__gte=date.today()).distinct()
         # generate report
         report = ''
         for patient in patients:
